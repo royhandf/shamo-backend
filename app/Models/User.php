a11fecbe4ec40169f54096380db3ba86cc63d08e
royhandf <royhandaffa14@gmail.com>
2023-04-14 13:26:51 +0700

    fixed model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
+        'phone',
+        'roles',
         'password',
     ];
 
